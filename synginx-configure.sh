983c43aa7d279cebdd099ef2ebd6c55a5241c152
Evgenii Klepilin <evgenii.klepilin@gmail.com>
2018-12-28 14:44:09 +0600

    Update synginx-configure.sh

--- synginx-configure.sh
+++ synginx-configure.sh
@@ -3,8 +3,9 @@ declare -a arr=("sudo cp test.demo.website.com /etc/nginx/sites-available/test.d
 #current directory: need to either specify full path for  test.demo.irokez or store it inside this file and create file from scratch
 #instead of copying it
 		"sudo ln -s ../sites-available/test.demo.website.com /etc/nginx/sites-enabled/test.demo.website.com" # Create symlink in sites-enabled/
-		"sudo sed -i '36s/.*/; listen = \/run\/php\/php7.2-fpm.sock/' /etc/php/7.2/fpm/pool.d/www.conf" # Comment previous setting
-		"sudo sed -i '37ilisten = 127.0.0.1:9000' /etc/php/7.2/fpm/pool.d/www.conf" # Add listen = 127.0.0.1:9000 line
+		## Uncomment these lines if you wish to set listening on a port 9000 manually
+		#"sudo sed -i '36s/.*/; listen = \/run\/php\/php7.2-fpm.sock/' /etc/php/7.2/fpm/pool.d/www.conf" # Comment previous setting
+		#"sudo sed -i '37ilisten = 127.0.0.1:9000' /etc/php/7.2/fpm/pool.d/www.conf" # Add listen = 127.0.0.1:9000 line
 		"sudo sed -i '1i127.0.0.99	test.demo.irokez.me' /etc/hosts" # Add another domain in hosts file
 		)
 
